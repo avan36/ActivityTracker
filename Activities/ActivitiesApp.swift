c8cd5c377bb97895cf094401b912760d70a8aa47
avan36 <158532304+avan36@users.noreply.github.com>
2024-07-08 15:36:20 -0700

    Initial Commit

---
+++ Activities/ActivitiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivitiesApp.swift
+//  Activities
+//
+//  Created by Ambrose V on 08/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct ActivitiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
